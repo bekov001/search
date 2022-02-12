c67b30e74a35ca93e74543643ea810f966bef1da
Anuar <ava200ava@gmail.com>
2022-02-12 22:34:31 +0600

    работал 3 часа

--- main.py
+++ main.py
@@ -1,55 +1,92 @@
 import sys
 from io import BytesIO
-# Этот класс поможет нам сделать картинку из потока байт
 
 import requests
 from PIL import Image
 
+# Этот класс поможет нам сделать картинку из потока байт
+
 # Пусть наше приложение предполагает запуск:
 # python search.py Москва, ул. Ак. Королева, 12
 # Тогда запрос к геокодеру формируется следующим образом:
-from hello import get_size
 
 toponym_to_find = " ".join(sys.argv[1:])
 
-geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
-geocoder_params = {
-    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-    "geocode": toponym_to_find,
-    "format": "json"}
+def get_coord(name):
+    geocoder_request = "http://geocode-maps.yandex.ru/1.x/"
+    params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": name,
+        "format": "json"
+    }
+    # Выполняем запрос.
+    response = requests.get(geocoder_request, params=params)
+    if response:
+        # Преобразуем ответ в json-объект
+        json_response = response.json()
 
-response = requests.get(geocoder_api_server, params=geocoder_params)
+        # Получаем первый топоним из ответа геокодера.
+        # Согласно описанию ответа, он находится по следующему пути:
+        toponym = \
+        json_response["response"]["GeoObjectCollection"]["featureMember"][0][
+            "GeoObject"]
+        # Полный адрес топонима:
+        toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"][
+            "text"]
+        # Координаты центра топонима:
+        toponym_coodrinates = toponym["Point"]["pos"]
+        # Печатаем извлечённые из ответа поля:
+        return toponym_coodrinates
+    raise ValueError("Неверный запрос")
 
-if not response:
-    # обработка ошибочной ситуации
-    pass
 
+search_api_server = "https://search-maps.yandex.ru/v1/"
+api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
-# Преобразуем ответ в json-объект
+address_ll = toponym_to_find
+if toponym_to_find and not toponym_to_find.split(",")[0].isdigit():
+    address_ll = ",".join(get_coord(toponym_to_find).split())
+search_params = {
+    "apikey": api_key,
+    "text": "аптека",
+    "lang": "ru_RU",
+    "ll": address_ll,
+    "type": "biz"
+}
+
+response = requests.get(search_api_server, params=search_params)
+
 json_response = response.json()
-# Получаем первый топоним из ответа геокодера.
-toponym = json_response["response"]["GeoObjectCollection"][
-    "featureMember"][0]["GeoObject"]
-# Координаты центра топонима:
-toponym_coodrinates = toponym["Point"]["pos"]
-# Долгота и широта:
-toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+# Получаем первую найденную организацию.
+organization = json_response["features"][0]
+# Название организации.
+org_name = organization["properties"]["CompanyMetaData"]["name"]
+org_hours = organization["properties"]["CompanyMetaData"]["Hours"]
+# Адрес организации.
+org_address = organization["properties"]["CompanyMetaData"]["address"]
 
-delta = get_size(toponym)
+# Получаем координаты ответа.
+point = organization["geometry"]["coordinates"]
+org_point = "{0},{1}".format(point[0], point[1])
+delta = "0.007"
+
 # Собираем параметры для запроса к StaticMapsAPI:
 map_params = {
-    "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": ",".join([delta, delta]),
+    # позиционируем карту центром на наш исходный адрес
+    # "ll": address_ll,
+    # "spn": ",".join([delta, delta]),
     "l": "map",
-    "pt": ",".join([toponym_longitude, toponym_lattitude])
+    # добавим точку, чтобы указать найденную аптеку
+    "pt": f"{org_point},pm2al~{address_ll},org"
 }
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 # ... и выполняем запрос
 response = requests.get(map_api_server, params=map_params)
+print("Адрес", org_address)
+print("Название", org_name)
+print("Время", org_hours["text"])
 
 Image.open(BytesIO(
     response.content)).show()
-# Создадим картинку
-# и тут же ее покажем встроенным просмотрщиком операционной системы
