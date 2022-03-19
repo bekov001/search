b3e12ccb938472c419692a8f093b4ce51472013d
Anuar <ava200ava@gmail.com>
2022-03-19 20:37:14 +0600

    great i did it

--- main.py
+++ main.py
@@ -10,46 +10,51 @@ from PIL import Image
 # Тогда запрос к геокодеру формируется следующим образом:
 from hello import get_size
 
-toponym_to_find = " ".join(sys.argv[1:])
 
-geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+def main():
+    toponym_to_find = " ".join(sys.argv[1:])
 
-geocoder_params = {
-    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-    "geocode": toponym_to_find,
-    "format": "json"}
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
-response = requests.get(geocoder_api_server, params=geocoder_params)
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": toponym_to_find,
+        "format": "json"}
 
-if not response:
-    # обработка ошибочной ситуации
-    pass
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if not response:
+        # обработка ошибочной ситуации
+        print("неправильный запрос\nпример запроса python search.py Москва, ул. Ак. Королева, 12")
+    display(response)
 
 
-# Преобразуем ответ в json-объект
-json_response = response.json()
-# Получаем первый топоним из ответа геокодера.
-toponym = json_response["response"]["GeoObjectCollection"][
-    "featureMember"][0]["GeoObject"]
-# Координаты центра топонима:
-toponym_coodrinates = toponym["Point"]["pos"]
-# Долгота и широта:
-toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+def display(response):
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+    # Получаем первый топоним из ответа геокодера.
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym["Point"]["pos"]
+    # Долгота и широта:
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
-delta = get_size(toponym)
-# Собираем параметры для запроса к StaticMapsAPI:
-map_params = {
-    "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": ",".join([delta, delta]),
-    "l": "map",
-    "pt": ",".join([toponym_longitude, toponym_lattitude])
-}
+    delta = get_size(toponym)
+    # Собираем параметры для запроса к StaticMapsAPI:
+    map_params = {
+        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        "spn": ",".join([delta, delta]),
+        "l": "map",
+        "pt": ",".join([toponym_longitude, toponym_lattitude])
+    }
 
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-# ... и выполняем запрос
-response = requests.get(map_api_server, params=map_params)
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    # ... и выполняем запрос
+    response = requests.get(map_api_server, params=map_params)
 
-Image.open(BytesIO(
-    response.content)).show()
-# Создадим картинку
-# и тут же ее покажем встроенным просмотрщиком операционной системы
+    Image.open(BytesIO(
+        response.content)).show()
+
+if __name__ == "__main__":
+    main()
