a74a899f4274ebe042a47ddad5305f80f6bbfc7d
Anuar <ava200ava@gmail.com>
2022-03-19 21:50:06 +0600

    ak anuar

--- main.py
+++ main.py
@@ -41,55 +41,66 @@ def get_coord(name):
     raise ValueError("Неверный запрос")
 
 
-search_api_server = "https://search-maps.yandex.ru/v1/"
-api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+def get_res():
+    search_api_server = "https://search-maps.yandex.ru/v1/"
+    api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
-address_ll = toponym_to_find
-if toponym_to_find and not toponym_to_find.split(",")[0].isdigit():
-    address_ll = ",".join(get_coord(toponym_to_find).split())
-search_params = {
-    "apikey": api_key,
-    "text": "аптека",
-    "lang": "ru_RU",
-    "ll": address_ll,
-    "type": "biz"
-}
+    address_ll = toponym_to_find
+    if toponym_to_find and not toponym_to_find.split(",")[0].isdigit():
+        address_ll = ",".join(get_coord(toponym_to_find).split())
+    search_params = {
+        "apikey": api_key,
+        "text": "аптека",
+        "lang": "ru_RU",
+        "ll": address_ll,
+        "type": "biz"
+    }
 
-response = requests.get(search_api_server, params=search_params)
-
-json_response = response.json()
-data = []
-for i in range(min(10, len(json_response["features"]))):
-    organization = json_response["features"][i]
-    # Название организации.
-    org_name = organization["properties"]["CompanyMetaData"]["name"]
-    org_hours = organization["properties"]["CompanyMetaData"].get("Hours", False)
-    org_address = organization["properties"]["CompanyMetaData"]["address"]
-
-    # Получаем координаты ответа.
-    point = organization["geometry"]["coordinates"]
-    org_point = "{0},{1}".format(point[0], point[1])
-    if org_hours:
-        if org_hours["Availabilities"][0].get("TwentyFourHours", False):
-            data.append(("pm2gnl", org_point))
-        else:
-            data.append(("org", org_point))
+    response = requests.get(search_api_server, params=search_params)
+    if response:
+        prepare_data(response, address_ll)
     else:
-        data.append(("pm2grl", org_point))
-    # Адрес организации.
-# Собираем параметры для запроса к StaticMapsAPI:
-map_params = {
-    # позиционируем карту центром на наш исходный адрес
-    # "ll": address_ll,
-    # "spn": ",".join([delta, delta]),
-    "l": "map",
-    # добавим точку, чтобы указать найденную аптеку
-    "pt": f"{'~'.join([','.join((i[1], i[0])) for i in data])}~{address_ll}"
-}
+        print("Некорректный запрос")
 
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-# ... и выполняем запрос
-response = requests.get(map_api_server, params=map_params)
-print(response.url)
-Image.open(BytesIO(
-    response.content)).show()
+
+def prepare_data(response, address_ll):
+    json_response = response.json()
+    data = []
+    for i in range(min(10, len(json_response["features"]))):
+        organization = json_response["features"][i]
+        org_hours = organization["properties"]["CompanyMetaData"].get("Hours", False)
+
+        # Получаем координаты ответа.
+        point = organization["geometry"]["coordinates"]
+        org_point = "{0},{1}".format(point[0], point[1])
+        if org_hours:
+            if org_hours["Availabilities"][0].get("TwentyFourHours", False):
+                data.append(("pm2gnl", org_point))
+            else:
+                data.append(("org", org_point))
+        else:
+            data.append(("pm2grl", org_point))
+        # Адрес организации.
+    display(data, address_ll)
+
+
+def display(data, address_ll):
+    # Собираем параметры для запроса к StaticMapsAPI:
+    map_params = {
+        # позиционируем карту центром на наш исходный адрес
+        # "ll": address_ll,
+        # "spn": ",".join([delta, delta]),
+        "l": "map",
+        # добавим точку, чтобы указать найденную аптеку
+        "pt": f"{'~'.join([','.join((i[1], i[0])) for i in data])}~{address_ll}"
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    # ... и выполняем запрос
+    response = requests.get(map_api_server, params=map_params)
+    Image.open(BytesIO(
+        response.content)).show()
+
+
+if __name__ == "__main__":
+    get_res()
